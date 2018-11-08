22476d776f1b6af1822703b4116cd695f97678ac
DuongTheChung <duongchung666@gmail.com>
2018-11-08 10:20:06 +0700

    Update firebase database final ,end project

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,14 +5,13 @@ import 'firebase/database';
 import 'firebase/storage';
  
 var config = {
-    apiKey: "AIzaSyA_Hi4eopZeOR0i29A0tgz58cGwnG6E7p8",
-    authDomain: "test-4e5bd.firebaseapp.com",
-    databaseURL: "https://test-4e5bd.firebaseio.com",
-    projectId: "test-4e5bd",
-    storageBucket: "test-4e5bd.appspot.com",
-    messagingSenderId: "1028848693551"
+    apiKey: "AIzaSyChL1K1XqWrE4IraJM3RodkBbuwPfdnQT4",
+    authDomain: "chat-app-mini.firebaseapp.com",
+    databaseURL: "https://chat-app-mini.firebaseio.com",
+    projectId: "chat-app-mini",
+    storageBucket: "chat-app-mini.appspot.com",
+    messagingSenderId: "781983620261"
   };
-
 firebase.initializeApp(config)
 
 export default firebase;
